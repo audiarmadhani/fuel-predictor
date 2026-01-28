24289ebdcb749247d99d28be624bc090e864309b
Audi Armadhani <audiarmadhani@aulias-macbook-air-7.tail113f3.ts.net>
2026-01-28 17:40:13 +0700

    frontend rev 15

--- backend/api/track.py
+++ backend/api/track.py
@@ -1,11 +1,15 @@
 from fastapi import APIRouter, Request
 from supabase import create_client
 import hashlib
+from dotenv import load_dotenv
+
+load_dotenv()  # ensures local .env works
+
 import os
 
 router = APIRouter()
 
-supabase = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_SERVICE_KEY"))
+supabase = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_SERVICE_ROLE_KEY"))
 
 @router.post("/track")
 async def track_visit(request: Request):
